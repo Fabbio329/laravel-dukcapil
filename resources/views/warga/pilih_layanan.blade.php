000bfe31a7ddfc95bc7d50a200660d94930436f9
Fabbio329 <fabioaja6@gmail.com>
2026-07-13 11:45:59 +0700

    nambah middleware login

--- resources/views/warga/pilih_layanan.blade.php
+++ resources/views/warga/pilih_layanan.blade.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Pilih Pelayanan</title>
+    <title>Pilih Pelayanan </title>
 </head>
 <body class="container mt-5">
     <h3 class="mb-4">Pilih Jenis Pelayanan Dukcapil</h3>
