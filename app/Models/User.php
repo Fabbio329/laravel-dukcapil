8760a6bad747c43d10fc8d7f0405dbb1afb039d1
Fanny <steffanyredmi@gmail.com>
2026-07-15 13:10:42 +0700

    tambah auth pada user di models ( app )

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -26,6 +26,16 @@ class User extends Authenticatable
     public function pengajuan()
     {
         return $this->hasMany(Pengajuan::class, 'user_id');
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isWarga(): bool
+    {
+        return $this->role === 'warga';
     }
 
     /**
